67ecffff40b2029ee4593b2683833c08eee57d54
Alexandros Bourantas <buritos@gmail.com>
2013-05-26 18:18:57 +0200

    added TrelloService.loadActionsForBoard(Board)

--- app/js/application.js
+++ app/js/application.js
@@ -145,6 +145,17 @@ var TrelloService = TrelloService || {};
 		function initBoardLists(TrelloLists) { Board.initTrelloLists(TrelloLists); }
 		Trello.get("boards/" + Board.id + "/lists", initBoardLists);
 	};
+	this.loadActionsForBoard = function(Board) {
+		function invokeBoardActions(TrelloBoard) {
+			var actions = _.reduceRight(TrelloBoard.actions, function(a,b) { a.push(b); return a; }, []);
+			Board.invokeTrelloActions(actions);
+		}
+		Trello.boards.get(Board.id, {
+			actions: 'createCard,deleteCard,updateCard', 
+			actions_limit: 1000,
+			actions_since: Board.timestamp.toISOString()
+		}, invokeBoardActions);
+	};
 }).apply(TrelloService);
 
 var session = session || {};
